92a8393027de575dee6519139839c578b20033ea
hori <hori.mock11@gmail.com>
2021-10-14 23:24:21 +0900

    クリック時にオブジェクトが生成されるように修正　リスト生成を関数化

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+"use strict";
+
+const tBody = document.getElementById("t-body");
 const newTask = document.getElementById("newTask");
 const addTask = document.getElementById("addTask");
+const tasks = [];
 
 addTask.addEventListener("click", () => {
   // 入力欄が空なら返す
   if (newTask.value === "") {
     return;
   }
-
   //tbodyの中の要素の数を取得し連番を取得する
-  const tBody = document.getElementById("t-body");
   const id = tBody.children.length;
 
   // コメントの値を取得する
@@ -16,37 +18,48 @@ addTask.addEventListener("click", () => {
 
   // 作業中ボタンを表示する
   const workStatus = document.createElement("button");
-  workStatus.innerText = "作業中";
+  workStatus.textContent = "作業中";
 
   // 削除ボタンを表示する
   const deleteButton = document.createElement("button");
-  deleteButton.innerText = "削除";
+  deleteButton.textContent = "削除";
 
-  // テーブルに表示する配列に格納
-  const todos = [id, comment, workStatus, deleteButton];
+  // todoリストのオブジェクトを生成
+  const todo = {};
+  todo.id = id;
+  todo.comment = comment;
+  todo.workStatus = workStatus;
+  todo.deleteButton = deleteButton;
 
-  // テーブルの行を用意
-  const tr = document.createElement("tr");
-
-  // 配列を処理する
-  todos.forEach((todo) => {
-    // テーブルの列を用意
-    const td = document.createElement("td");
-
-    if (todo instanceof HTMLElement) {
-      // buttonタグの場合
-      td.appendChild(todo);
-    } else {
-      td.textContent = todo;
-    }
-
-    // 行に列を追加する
-    tr.appendChild(td);
-  });
-
-  // テーブルの行を追加する
-  tBody.appendChild(tr);
-
-  // 入力欄を空にする
+  // オブジェクトを配列に格納
+  tasks.push(todo);
+  showTodo();
   newTask.value = "";
 });
+
+const showTodo = () => {
+  while (tBody.firstChild) {
+    tBody.textContent = "";
+  }
+  // 配列を処理
+  tasks.forEach((task) => {
+    const tr = document.createElement("tr");
+    const tdId = document.createElement("td");
+    const tdComment = document.createElement("td");
+    const tdWorkStatus = document.createElement("td");
+    const tdDeleteButton = document.createElement("td");
+
+    tdId.textContent = task.id;
+    tdComment.textContent = task.comment;
+    const statusButton = task.workStatus;
+    const deleteBtn = task.deleteButton;
+
+    tBody.appendChild(tr);
+    tr.appendChild(tdId);
+    tr.appendChild(tdComment);
+    tr.appendChild(tdWorkStatus);
+    tdWorkStatus.appendChild(statusButton);
+    tr.appendChild(tdDeleteButton);
+    tdDeleteButton.appendChild(deleteBtn);
+  });
+};
